666198f62cb519d36d539e71dfcfb3df93c77837
Kaloyy-Ortiaga <90772416+Kaloyy-Ortiaga@users.noreply.github.com>
2024-10-07 16:22:11 +0800

    Add_employee_form

--- Employee/views.py
+++ Employee/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .forms import EmployeeForm
+from .forms import AddEmployeeForm
 
-def employee_form(request):
-    form = EmployeeForm()
-    return render(request, 'Employee/employee_form.html', {'form': form})
+def Add_employee_form(request):
+    form = AddEmployeeForm()
+    return render(request, 'Employee/Add_employee_form.html', {'form': form})
